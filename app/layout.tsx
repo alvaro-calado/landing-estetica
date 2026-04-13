b3bc08e9f8f9a36a5d4ced175007786876d686ac
alvaro-calado <gustavocalado4321@gmail.com>
2026-04-13 17:56:11 -0300

    adicionando metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
 import "./globals.css";
+
+export const metadata = {
+  title: "Biomedicina estética",
+  description: "Procedimentos estéticos premium",
+  icons: {
+    icon: "/icon.png",
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
